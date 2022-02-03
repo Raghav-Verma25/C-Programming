c6305a18fe8907253fc97bd59f727b02cc098626
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-02-03 22:58:07 +0530

    Create SwitchSelectionStatment.c

---
+++ SwitchSelectionStatment.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+                   // switch selection statment
+            //point to be know that ASCII Value if a to z-97 to 122 
+                  //and A to Z - 65 to 90
+    char i=66;
+    int a=66;
+
+    switch(i)
+    {
+
+case 'W':printf("case 1 executed");
+     break;
+     
+case 'B':printf("\ncase 2 executed");
+     break;
+
+default: printf("\ndefault case ");
+     }
+}
